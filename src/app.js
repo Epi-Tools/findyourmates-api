7e1cadd79aadb7d75db997257e902065471e94d9
Yann Carlen <yann.carlen@epitech.eu>
2017-05-25 20:24:29 +0200

    add deps

--- src/app.js
+++ src/app.js
@@ -1,9 +1,15 @@
 const express = require('express')
 const helmet = require('helmet')
 const roomsAPI = require('./api/rooms')
+const bodyParser = require('body-parser')
+const cors = require('cors')
 const app = express()
+require('dotenv').config()
 
 app.use(helmet())
+app.use(bodyParser.json())
+app.use(bodyParser.urlencoded({ extended: false }))
+app.use(cors())
 
 const wesh = msg => console.log(msg)
 const port = process.env.PORT || 8000
